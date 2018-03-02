a249406c9058a12ad65bc343055a74e8e577f03e
Maverick Tesini <mavericktesini@gmail.com>
2018-03-02 21:20:50 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(array)  #some code block to be executed at call time
+  if block_given?
+    i = 0
+    while i < array.length
+      dostuff
+    end
+  else
+    "You didn't provide a code block"
+  end
+end
 
