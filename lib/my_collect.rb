c984994bb638fb0d1d43d5333bcb4e6a2229d60a
Maverick Tesini <mavericktesini@gmail.com>
2018-03-02 21:21:30 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,10 +2,10 @@ def my_collect(array)  #some code block to be executed at call time
   if block_given?
     i = 0
     while i < array.length
-      dostuff
+      yield(array[i])
+      i += 1
     end
   else
     "You didn't provide a code block"
   end
 end
-
